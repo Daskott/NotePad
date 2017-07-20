925776f2245ebf838893e756cd6bb07ae2460823
edmondcotterell <edmondcotterell@gmail.com>
2017-07-20 05:11:42 -0500

    updated server/index

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const express = require('express');
 const staticRouter = require('./static');
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
 var apiRouter = require('./routes');
 var models = require('../models');
